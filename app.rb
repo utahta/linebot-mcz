182d2e10a80e124d9950cbcc4840d4c5ca917ed4
utahta <labs.ninxit@gmail.com>
2016-04-11 01:16:51 +0900

    add ありやす

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ post '/callback' do
     search_word = member.sample if msg['content']['text'].match(/誰(か|でも)/)
     search_word = member[0] if msg['content']['text'].match(/玉井|[たタ][まマ][いイ]|[しシ][おオ][りリ][んン]?|詩織|玉さん|[たタ][まマ]さん/)
     search_word = member[1] if msg['content']['text'].match(/百田|[もモ][もモ][たタ]|[夏かカ][菜なナ][子こコ]/)
-    search_word = member[2] if msg['content']['text'].match(/有安|[もモ][もモ][かカ]|杏果/)
+    search_word = member[2] if msg['content']['text'].match(/有安|[あア][りリ][やヤ][すス]|[もモ][もモ][かカ]|杏果/)
     search_word = member[3] if msg['content']['text'].match(/佐々木|[さサ][さサ][きキ]|[あア][やヤ][かカ]|彩夏|[あア]ー[りリ][んン]/)
     search_word = member[4] if msg['content']['text'].match(/高城|[たタ][かカ][ぎギ]|[れレ][にニ]/)
 
