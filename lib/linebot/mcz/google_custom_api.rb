40bef3f604cb23ad43a41c834f6e785a5a0e3abc
utahta <labs.ninxit@gmail.com>
2016-04-14 19:43:10 +0900

    add retry

--- lib/linebot/mcz/google_custom_api.rb
+++ lib/linebot/mcz/google_custom_api.rb
@@ -10,16 +10,20 @@ module Linebot
       end
 
       def search_image(word)
-        response = @conn.get do |req|
-          req.params['cx'] = ENV.fetch('GOOGLE_CUSTOM_API_ID')
-          req.params['key'] = ENV.fetch('GOOGLE_CUSTOM_API_KEY')
-          req.params['q'] = word
-          req.params['searchType'] = 'image'
-          req.params['num'] = 10
-          req.params['start'] = rand(20)
+        content = nil
+        3.times do
+          response = @conn.get do |req|
+            req.params['cx'] = ENV.fetch('GOOGLE_CUSTOM_API_ID')
+            req.params['key'] = ENV.fetch('GOOGLE_CUSTOM_API_KEY')
+            req.params['q'] = word
+            req.params['searchType'] = 'image'
+            req.params['num'] = 10
+            req.params['start'] = rand(20)
+          end
+          content = JSON.parse(response.body)
+          break unless content['items'].nil?
         end
 
-        content = JSON.parse(response.body)
         10.times do
           item = content['items'].sample
           if item['mime'] == 'image/jpeg'
